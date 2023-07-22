cafe40398643a51ec8377e585a65615fe2c02d80
Jonas Gschwend <37105458+gschwendj@users.noreply.github.com>
2023-07-22 14:29:27 +0200

    test script with schedule

--- test.py
+++ test.py
@@ -1,16 +1,22 @@
+import logging
+import time
 from gpiozero import LED, DigitalInputDevice, DigitalOutputDevice
-from pause import seconds
+import schedule
+
 from src.level_alarm import Level_alarm
 from src.pump_control import Pump_control
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     pump_out = DigitalOutputDevice(5)
-    indicator_led__out = LED(3)
-    level_alarm_in = DigitalInputDevice(4)
+    indicator_led__out = LED(6)
+    level_alarm_in = DigitalInputDevice(4, bounce_time=0.1)
 
     pump_control = Pump_control(pump_out, level_alarm_in)
     leve_alarm = Level_alarm(level_alarm_in, indicator_led__out, pump_out)
 
+    schedule.every().day.at("20:00").do(pump_control.irrigation, litre=10)
+
     while True:
-        seconds(20)
-        pump_control.irrigation(10)
+        schedule.run_pending()
+        time.sleep(1)
