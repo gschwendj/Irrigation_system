fbecc01aed3331aa23719b9178b47ba2f4814f07
Jonas Gschwend <37105458+gschwendj@users.noreply.github.com>
2023-07-25 18:02:57 +0200

    add pump volume to pump class

--- src/pump_control.py
+++ src/pump_control.py
@@ -8,13 +8,18 @@ class Pump_control:
         self,
         water_pump: DigitalOutputDevice,
         level_sensor: DigitalInputDevice,
+        pump_volume: int,
     ) -> None:
         self.pump_output = water_pump
         self.level_sensor = level_sensor
+        self.pump_volume = pump_volume
 
     def status(self) -> bool:
         logging.debug("Pump value is: {}".format(self.pump_output.value))
         return bool(self.pump_output.value)
+
+    def set_pump_volume(self, litre: int):
+        self.pump_volume = litre
 
     def start(self) -> bool:
         # check that tank is not empty
@@ -27,12 +32,12 @@ class Pump_control:
         self.pump_output.off()
         logging.debug("Pump value is: {}".format(self.pump_output.value))
 
-    def irrigation(self, litre: int) -> None:
+    def irrigation(self) -> None:
         if self.start():
             logging.debug(
                 "pump is pumping for {:.2f} seconds / {} litre".format(
-                    litre / 0.1125, litre
+                    self.pump_volume / 0.1125, self.pump_volume
                 )
             )
-            sleep(litre / 0.1125)
+            sleep(self.pump_volume / 0.1125)
             self.stop()
