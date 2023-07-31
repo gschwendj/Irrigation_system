fbbc62ab9973edfa5fc0d77b8cc09819cb1d15d7
Jonas Gschwend <37105458+gschwendj@users.noreply.github.com>
2023-07-31 15:09:19 +0200

    first try for cli

--- cli/cli.py
+++ cli/cli.py
@@ -1,12 +1,56 @@
+from sys import exit
+import logging
 import socket
+import argparse
+import json
 
-if __name__ == "__main__":
-    HOST = "backend"  # The server's hostname or IP address
-    PORT = 5000  # The port used by the server
+
+def main():
+    host = "localhost"
+    port = 5000
+    logging.basicConfig(level=logging.DEBUG)
+
+    parser = argparse.ArgumentParser(
+        prog="IrrigationCtrl", description="CLI for the Irrigation System."
+    )
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        "-s", "--start", action="store_true", help="starts the irrigation process"
+    )
+    group.add_argument(
+        "-i",
+        "--status",
+        action="store_true",
+        help="returns the status of the irrigation system",
+    )
+    group.add_argument(
+        "-v",
+        "--volume",
+        action="store",
+        type=int,
+        help="set the volume in litre that gets pumped during the irrigation process",
+        metavar="V",
+    )
+
+    args = parser.parse_args()
+    if args.volume != None:
+        send_cmd = json.dumps({"command": "set pump volume", "volume": args.volume})
+        print(f"Set pump volume to {args.volume} litres")
+    elif args.start:
+        send_cmd = json.dumps({"command": "start"})
+        print("start irrigation process")
+    elif args.status:
+        send_cmd = json.dumps({"command": "status"})
+        print("get status of irrigation system")
+    else:
+        exit(0)
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        s.sendall(b"Hello, world")
-        data = s.recv(1024)
+        s.connect((host, port))
+        s.sendall(send_cmd.encode())
+        recv_reply = s.recv(1024)
+        logging.debug(recv_reply)
 
-    print(f"Received {data!r}")
+
+if __name__ == "__main__":
+    main()
